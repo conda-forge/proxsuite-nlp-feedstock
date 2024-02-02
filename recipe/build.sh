05e0028f5417aa635d39de9ae18e792e6cce9488
Olivier Roussel <olivier.roussel@inria.fr>
2024-02-02 12:00:59 +0100

    Do not generate python stubs when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,24 @@ rm -rf build
 mkdir build
 cd build
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+export GENERATE_PYTHON_STUBS=1
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export GENERATE_PYTHON_STUBS=0
+fi
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=$PYTHON \
       -DBUILD_PYTHON_INTERFACE=ON \
-      -DGENERATE_PYTHON_STUBS=ON \
+      -DGENERATE_PYTHON_STUBS=$GENERATE_PYTHON_STUBS \
       -DBUILD_EXAMPLES=ON \
       -DBUILD_BENCHMARK=OFF \
       -DBUILD_TESTING=OFF
@@ -19,3 +32,8 @@ cmake --build . --parallel ${CPU_COUNT}
 
 # install
 cmake --build . --parallel ${CPU_COUNT} --target install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+fi
